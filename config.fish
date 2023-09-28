c5035e5d6cfdc4b3c5654e9df00b5c8c21605059
jasper <jasper@mail.de>
2023-09-28 10:40:41 +0200

    autoupdate

--- config.fish
+++ config.fish
@@ -1,8 +1,6 @@
 if status is-interactive
     set fish_greeting
 
-    # Commands to run in interactive sessions can go here
     abbr --add py3 python3
-    #abbr --add vim nvim
     alias vim nvim
 end
---
+++ update.fish
@@ -0,0 +1,9 @@
+set files ~/.tmux.conf ~/.config/fish/config.fish ~/.config/nvim/init.vim
+
+for f in files:
+    cp f .
+end
+
+git add *
+git commit -m "autoupdate"
+git push
